f2bb07057c7a0aa04667559319914dfbe266ef69
yeooonee <skylarlee1003@gmail.com>
2026-09-16 15:23:17 +0900

    06 정리

--- Data-Structures/Stack_and_Queue/Q6_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q6_C_SQ.c
@@ -111,13 +111,12 @@ int main()
 
 void removeUntil(Stack *s, int value)
 {
-	int i, j, len = s->ll.size, val, len2;
+	int i, len = s->ll.size, val;
 
 	for (i = 0; i< len; i++){
 		val = pop(s);
 		
 		if (val == value){
-			len2 = s->ll.size;
 			push(s, val);
 			return;
 		}
